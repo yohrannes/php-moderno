7051a899132cf2df898462998e22220b1117dfb0
yohrannes <yohrannes@gmail.com>
2023-07-29 06:40:00 -0300

    test

--- desafios/d008/index.php
+++ desafios/d008/index.php
@@ -6,28 +6,5 @@
     <link rel="stylesheet" href="style.css">
     <title>Desafio PHP</title>
 </head>
-<body>
-    <?php 
-        $numero = $_GET['num'] ?? 1;
-    ?>
-    <main>
-        <h1>Informe um número</h1>
-        <form action="<?=$_SERVER['PHP_SELF']?>" method="get">
-        <label for="sal">Número</label>
-        <input type="number" name="num" id="num" value="<?=$numero?>">
-        <input type="submit" value="Calcular Raízes">
-        </form>
-    </main>
-    <section>
-        <h2>Resultado Final</h2>
-        <?php 
-        $rq = $numero ** (1/2);
-        $rc = $numero ** (1/3);
-
-        echo "<p>Analizando o <strong>numero $numero</strong>, temos:";
-        echo "<ul><li>A sua raiz quadrada é ". number_format($rq, 3, ",", ".") ."</li>";
-        echo "<li> a sua raiz cúbica é ". number_format($rc,  3, ",", ".") ."</li></ul>";
-        ?>
-    </section>
 </body>
 </html>
