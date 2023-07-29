600eb6b74ed7015b2cce8e23a4532d7839539dd1
yohrannes <yohrannes@gmail.com>
2023-07-29 07:58:05 -0300

    Finalizando desafio de cálculo de media aritmetica e media ponderada

--- desafios/d009/index.php
+++ desafios/d009/index.php
@@ -2,32 +2,43 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=7">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="style.css">
     <title>Desafio PHP</title>
 </head>
 <body>
     <?php 
-        $numero = $_GET['num'] ?? 1;
+        $valor1 = $_GET['v1'] ?? '';
+        $peso1 = $_GET['p1'] ?? '';
+        $valor2 = $_GET['v2'] ?? '';
+        $peso2 = $_GET['p2'] ?? '';
     ?>
     <main>
-        <h1>Informe um número</h1>
-        <form action="<?=$_SERVER['PHP_SELF']?>" method="get">
-        <label for="sal">Número</label>
-        <input type="number" name="num" id="num" value="<?=$numero?>">
-        <input type="submit" value="Calcular Raízes">
+        <h1>Médias Aritméticas</h1>
+            <form action="<?=$_SERVER['PHP_SELF']?>" method="get">
+            <label for="v1">1° Valor</label>
+            <input type="number" name="v1" id="v1" required value="<?=$valor1?>">
+            <label for="v1">1° Peso</label>
+            <input type="number" name="p1" id="p1" min="1" required value="<?=$peso1?>">
+            <label for="v1">2° Valor</label>
+            <input type="number" name="v2" id="v2" required value="<?=$valor2?>">
+            <label for="v1">2° Peso</label>
+            <input type="number" name="p2" id="p2" min="1" required value="<?=$peso2?>">
+            <input type="submit" value="Calcular Médias">
         </form>
     </main>
     <section>
-        <h2>Resultado Final</h2>
-        <?php 
-        $rq = $numero ** (1/2);
-        $rc = $numero ** (1/3);
-
-        echo "<p>Analizando o <strong>numero $numero</strong>, temos:";
-        echo "<ul><li>A sua raiz quadrada é ". number_format($rq, 3, ",", ".") ."</li>";
-        echo "<li> a sua raiz cúbica é ". number_format($rc,  3, ",", ".") ."</li></ul>";
+        <?php
+            $ma = ($valor1 + $valor2) / 2;
+            $mp = ($valor1 * $peso1 + $valor2 * $peso2)/($peso1 + $peso2);
         ?>
+        <h2>Cálculo das Médias</h2>
+        <p>Analisando os valores <?=$valor1?> e <?=$valor2?>:</p>
+        <ul>
+            <li>A <strong>Média Aritmética Simples</strong> entre os valores é igual a <?=number_format($ma,2,",",".")?>.</li>
+            <li>A <strong>Média Aritmética Ponderada</strong> com pesos <?=$peso1?> e <?=$peso2?> é igual a <?=number_format($mp,2,",",".")?>.</li>
+        </ul>
     </section>
 </body>
 </html>
